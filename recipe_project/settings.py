58a50c0d8161cdba5723c01af424a142cb95cf2b
mihadereani <110335459+mihadereani@users.noreply.github.com>
2023-06-07 01:17:39 +0200

    database

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -140,5 +140,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
